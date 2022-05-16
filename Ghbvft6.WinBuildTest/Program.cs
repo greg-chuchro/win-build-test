0a62523f72c3d634b7f4db58588d618f9b6b70c2
greg-chuchro <gchuchro@acm.org>
2022-05-16 13:45:16 +0900

    init

---
+++ Ghbvft6.WinBuildTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ghbvft6.WinBuildTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
